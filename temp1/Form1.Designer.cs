c36813005d5de74f7460537bde878f7a9f507f8b
Muhammad Ali <muhammad.ali.ch@hotmail.com>
2017-02-27 00:09:53 -0600

    Copy file faster
    a snippet of copying file from one folder to another using C#

---
+++ temp1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace temp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(103, 115);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(282, 29);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(320, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Source";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Dest";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "s";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Done!!!!!!!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(397, 161);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.94D;
+            this.ShowInTaskbar = false;
+            this.Text = "Copy_Paste";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ temp1/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace temp1
+{
+    public partial class Form1 : Form
+    {
+        bool a = false;
+        long temp = 0;
+        string dest = null;
+        string name = null;
+        string sourcepath = null;
+        FileStream obj1;
+        Thread t1;
+        Thread t2;
+        Thread t3;
+        Thread t4;
+        Thread t5;
+        Thread t6;
+        Thread t7;
+        Thread t8;
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            progressBar1.Hide();
+            label1.Hide();
+        }
+        public void read1(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+            obj.Read(byffer, 0, byffer.Length);
+            obj1.Write(byffer, 0, byffer.Length);
+            progressBar1.Increment( progressBar1.Value+(int)i);
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+        public void read2(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+                obj.Seek(i, SeekOrigin.Begin);
+                obj.Read(byffer, 0, byffer.Length);
+                obj1.Seek(i, SeekOrigin.Begin);
+                obj1.Write(byffer, 0, byffer.Length);
+                progressBar1.Increment(progressBar1.Value + (int)i);
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+        public void read3(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+            long op = i + i;
+            obj.Seek(op, SeekOrigin.Begin);
+            obj.Read(byffer, 0, byffer.Length);
+            long i1 = i + i;
+     
+                obj1.Seek(i1, SeekOrigin.Begin);
+                obj1.Write(byffer, 0, byffer.Length);
+                progressBar1.Increment(progressBar1.Value + (int)i);
+         
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+        public void read4(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+            long op = i + i + i;
+            obj.Seek(op, SeekOrigin.Begin);
+            obj.Read(byffer,0,byffer.Length);
+            long i1 = i + i + i;
+            obj1.Seek(i1, SeekOrigin.Begin);
+            obj1.Write(byffer, 0, byffer.Length);
+            progressBar1.Increment(progressBar1.Value + (int)i);    
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+        public void read5(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+            long op = i + i + i+i;
+            obj.Seek(op, SeekOrigin.Begin);
+            obj.Read(byffer, 0, byffer.Length);
+            long i1 = i + i + i+i;
+            obj1.Seek(i1, SeekOrigin.Begin);
+            obj1.Write(byffer, 0, byffer.Length);
+            progressBar1.Increment(progressBar1.Value + (int)i);
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+        public void read6(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+            long op = i + i + i+i+i;
+            obj.Seek(op, SeekOrigin.Begin);
+            obj.Read(byffer, 0, byffer.Length);
+            long i1 = i + i + i+i+i;
+       
+                obj1.Seek(i1, SeekOrigin.Begin);
+                obj1.Write(byffer, 0, byffer.Length);
+                progressBar1.Increment(progressBar1.Value + (int)i);
+   
+
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+        public void read7(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+            long op = i + i + i + i + i + i;
+            obj.Seek(op, SeekOrigin.Begin);
+            obj.Read(byffer, 0, byffer.Length);
+            long i1 = i + i + i + i + i + i;
+                obj1.Seek(i1, SeekOrigin.Begin);
+                obj1.Write(byffer, 0, byffer.Length);
+                progressBar1.Increment(progressBar1.Value + (int)i);
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+        public void read8(long i)
+        {
+            long temp2 = 0;
+            FileStream obj = File.OpenRead(sourcepath);
+            byte[] byffer = new byte[i];
+            long op = i + i + i + i + i + i+i;
+            obj.Seek(op, SeekOrigin.Begin);
+            obj.Read(byffer, 0, byffer.Length);
+            long i1 = i + i + i + i + i + i+i;
+                obj1.Seek(i1, SeekOrigin.Begin);
+                obj1.Write(byffer, 0, byffer.Length);
+                progressBar1.Increment(progressBar1.Value + (int)i);
+
+            obj.Close();
+            //if (progressBar1.Value == progressBar1.Maximum)
+            //{
+            //    progressBar1.Hide();
+            //    label1.Show();
+            //}
+        }
+
+        public void findName()
+        {
+            int temp = sourcepath.LastIndexOf(@"\");
+            name = sourcepath.Substring(temp + 1);
+        }
+        public void progress()
+        {
+            while (true)
+            {
+                if (t1.IsAlive == false && t2.IsAlive == false && t3.IsAlive == false && t4.IsAlive == false && t5.IsAlive == false && t6.IsAlive == false && t7.IsAlive == false && t8.IsAlive == false)
+                {
+                    progressBar1.Hide();
+                    label1.Text = "Doneeeee!!!!!";
+                    label1.Show();
+                    break;
+                }
+            
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            obj1 = File.Open(dest, FileMode.OpenOrCreate);
+            progressBar1.Show();
+            label1.Hide();
+            try
+            {
+                FileStream obj = File.OpenRead(sourcepath);
+                progressBar1.Maximum = (int)obj.Length;
+                long size_of_buffer = obj.Length / 8;
+       t1=          new Thread(() => { read1(size_of_buffer); });
+     t2 = new Thread(() => { read2(size_of_buffer); });
+       t3 = new Thread(() => { read3(size_of_buffer); });
+      t4 = new Thread(() => { read4(size_of_buffer); });
+      t5 = new Thread(() => { read5(size_of_buffer); });
+      t6 = new Thread(() => { read6(size_of_buffer); });
+       t7 = new Thread(() => { read7(size_of_buffer); });
+      t8 = new Thread(() => { read8(size_of_buffer); });
+     
+      t1.Start();
+      t2.Start();
+      t3.Start();
+      t4.Start();
+      t5.Start();
+      t6.Start();
+      t7.Start();
+      t8.Start();
+   
+      
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+            new Thread(progress).Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                sourcepath = openFileDialog1.FileName.ToString();
+                textBox1.Text = sourcepath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            findName();
+            DialogResult dr = folderBrowserDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath+name;
+                dest = folderBrowserDialog1.SelectedPath+name;
+            }
+        }
+    }
+}
